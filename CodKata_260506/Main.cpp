a611f28f4885ddda505066604f2e2a519d5ca5ad
MinniM <smikim85@gmail.com>
2026-05-07 09:49:46 +0900

    26년 05월 07일 코드카타 완료.

---
+++ CodKata_260506/Main.cpp
@@ -0,0 +1,18 @@
+//Main.cpp
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "Odd";
+    if (num % 2 == 0) {
+        answer = "Even";
+    }
+    else {
+        answer = "Odd";
+    }
+
+    return answer;
+}
